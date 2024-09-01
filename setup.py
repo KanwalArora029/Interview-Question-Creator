cebcdac7414efd5a6e58cc58c9baefa987d894c2
Kanwal Arora <kanwalarora029@gmail.com>
2024-09-01 18:08:30 +0100

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Interview Question Creator',
+    version='0.1',
+    author="Kanwal Arora",
+    author_email="kanwalarora029@gmail.com",  # Add your email here
+    packages=find_packages(),
+    install_requires = []
+)
